b25d9ca78fd3c98db8203a5ff691c42a6893f815
David Teixeira <david.teixeira31@gmail.com>
2019-05-10 10:08:08 +0200

    Modify signup

--- php/login.php
+++ php/login.php
@@ -40,7 +40,7 @@
             <input class="login-password" type="password" name="password" placeholder="Password">
             <input class="login-submit" type="submit" value="Login">
 
-            <a href="">Vous n'avez pas de compte créez en un !<br></a>
+            <a href="signup.php">Vous n'avez pas de compte créez en un !<br></a>
             <a href="">Mot de passe oublié ?<br> </a>
         </form>
 
